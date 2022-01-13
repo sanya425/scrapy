fc8ca036db2cfa6e7937adec225c165e466e57a7
sanya425 <sashaasu03@gmail.com>
2022-01-13 14:23:37 +0200

    gd_blog_v3

--- GridBlog/report.py
+++ GridBlog/report.py
@@ -1,6 +1,8 @@
 import subprocess
 import logging
 import os
+import sqlite3
+from sqlite3 import Error
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger('root')
 '''
@@ -16,25 +18,29 @@ else:
     process_authors = subprocess.Popen(['scrapy', 'crawl', 'authors'])
     process_articles = subprocess.Popen(['scrapy', 'crawl', 'articles'])
 '''
+conn = None
+try:
+    conn = sqlite3.connect('GDblog.db')
+except Error as e:
+    print(e)
+cur = conn.cursor()
 
-'''
-if tables exists:
-    cnt = max(id_articles)
+if cur.execute("""SELECT count(*) FROM sqlite_master WHERE type='table' AND name='table_name'"""):
+    cnt = ...
     last_date = ...
     os.system("scrapy crawl articles")
-    if cnt == max(id_articles):
+    if cnt == ...:
         print("There are no new blog posts since the last date")
     else:
         os.system("scrapy crawl authors")
 
 else:
+    logger.debug('start spiders')
     process_authors = subprocess.Popen(['scrapy', 'crawl', 'authors'])
     process_articles = subprocess.Popen(['scrapy', 'crawl', 'articles'])
-'''
+    process_authors.wait()
+    process_articles.wait()
 
-logger.debug('start spiders')
-process_authors = subprocess.Popen(['scrapy', 'crawl', 'authors'])
-process_articles = subprocess.Popen(['scrapy', 'crawl', 'articles'])
 
-process_authors.wait()
-process_articles.wait()
+
+
